446467ff0b034865212c1a4dc8caf5d203cb3c20
Dmitry Romanov <romanovda.nti@yandex.ru>
2024-09-25 18:55:37 +0300

    Lab1 with my own converter toRoman

--- src/main/java/ru/mpei/MyCalculator/RomanCalcs.java
+++ src/main/java/ru/mpei/MyCalculator/RomanCalcs.java
@@ -1,6 +1,5 @@
 package ru.mpei.MyCalculator;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class RomanCalcs extends Calculations{
@@ -18,13 +17,13 @@ public class RomanCalcs extends Calculations{
         if (operation == OperationType.addition){
             addition();
         }
-        if (operation == OperationType.subtraction){
+        else if (operation == OperationType.subtraction){
             subtraction();
         }
-        if (operation == OperationType.multiplication){
+        else if (operation == OperationType.multiplication){
             multiplication();
         }
-        if (operation == OperationType.division) {
+        else if (operation == OperationType.division) {
             division();
         }
     }
@@ -32,25 +31,25 @@ public class RomanCalcs extends Calculations{
     @Override
     public void addition() {
         int result = this.arabNumbers[0] + this.arabNumbers[1];
-        System.out.println(toRoman(result));
+        System.out.println(toRomanSecond(result));
     }
 
     @Override
     public void subtraction() {
         int result = this.arabNumbers[0] - this.arabNumbers[1];
-        System.out.println(toRoman(result));
+        System.out.println(toRomanSecond(result));
     }
 
     @Override
     public void multiplication() {
         int result = this.arabNumbers[0] * this.arabNumbers[1];
-        System.out.println(toRoman(result));
+        System.out.println(toRomanSecond(result));
     }
 
     @Override
     public void division() {
         int result = this.arabNumbers[0] / this.arabNumbers[1];
-        System.out.println(toRoman(result));
+        System.out.println(toRomanSecond(result));
     }
 
     private void toArabic(){
@@ -86,6 +85,45 @@ public class RomanCalcs extends Calculations{
 
         return roman.toString();
 
-        }
     }
 
+   public static String toRomanSecond(int result){
+        if (result <= 0){
+            return "No positive result";
+        }
+        String[] romanLiterals = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        int nTimesLit = 0;
+
+        StringBuilder roman = new StringBuilder();
+
+        nTimesLit = result / 100;
+        roman.append(romanLiterals[0].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 100;
+        nTimesLit = result / 90;
+        roman.append(romanLiterals[1].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 90;
+        nTimesLit = result / 50;
+        roman.append(romanLiterals[2].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 50;
+        nTimesLit = result / 40;
+        roman.append(romanLiterals[3].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 40;
+        nTimesLit = result / 10;
+        roman.append(romanLiterals[4].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 10;
+        nTimesLit = result / 9;
+        roman.append(romanLiterals[5].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 9;
+        nTimesLit = result / 5;
+        roman.append(romanLiterals[6].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 5;
+        nTimesLit = result / 4;
+        roman.append(romanLiterals[7].repeat(Math.max(0, nTimesLit)));
+        result = result - nTimesLit * 4;
+        roman.append(romanLiterals[8].repeat(Math.max(0, result)));
+
+        return roman.toString();
+
+    }
+}
